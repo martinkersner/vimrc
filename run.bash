8b787a4dd1e1842a43f6abd14348f72f2bc089cc
Martin Keršner <m.kersner@gmail.com>
2015-11-27 12:07:28 +0900

    bug fix; wrong name of variable

--- run.bash
+++ run.bash
@@ -4,5 +4,5 @@
 
 TINYMODE_ZIP="tinymode.zip"
 TINYMODE_LINK="http://www.vim.org/scripts/download_script.php?src_id=9130"
-wget -O "$TINYMODE"  "$TINYMODE_LINK"
-unzip "$TINYMODE"
+wget -O "$TINYMODE_ZIP"  "$TINYMODE_LINK"
+unzip "$TINYMODE_ZIP"
